31ff1947927cd3ff2ebf0f4712f7b63ce84ccfbc
Finn Marquardt <98032358+ERR0RW0LF@users.noreply.github.com>
2023-12-04 11:53:05 +0000

    Fix barcode width calculation

--- barcode/generator.py
+++ barcode/generator.py
@@ -107,7 +107,7 @@ def barcode_image_generator(number):
         barcode_height = 50
         barcode_image = Image.new('RGB', (barcode_width, barcode_height), 'white')
         draw = ImageDraw.Draw(barcode_image)
-        for i in range(barcode_widt/2):
+        for i in range(barcode_width/2):
             if barcode[i] == 1:
                 draw.line((i*2, 0, i*2, barcode_height), fill='black')
         return barcode_image
