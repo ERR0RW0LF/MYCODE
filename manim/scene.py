caa078092e0c35942b8a7a7e75423b2914ee54b3
ERR0RW0LF <finn.woma@gmail.com>
2024-04-15 14:21:06 +0200

    Add SquareToCircle scene and related animations

--- manim/scene.py
+++ manim/scene.py
@@ -6,3 +6,17 @@ class CreateCircle(Scene):
         circle.set_fill(RED, opacity=0.5)
         self.play(Create(circle))
         self.wait(1)
+
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+        
+
+        square = Square()  # create a square
+        square.rotate(PI / 4)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
