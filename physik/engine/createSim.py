bc29108a241b19ea5f5d4074a92a86dbb24a7d8a
ERR0RW0LF <finn.woma@gmail.com>
2024-04-30 18:53:22 +0200

    feat: Implement gravitational force calculation in Simulation class

--- physik/engine/createSim.py
+++ physik/engine/createSim.py
@@ -2,9 +2,11 @@ import sys
 import os
 import numpy as np
 import json
+import matplotlib.pyplot as plt
 
 #MARK: - Simulation class
 class Simulation():
+    # MARK: - Simulation properties
     def __init__(self):
         self.objects = {} # dictionary of objects with their properties (mass, position, velocity, constraints, etc.) in the simulation
         self.generalForces = [] # forces that apply to all objects
@@ -17,6 +19,7 @@ class Simulation():
         self.simSpace = None # the simulation space as a np.array (e.g. 3D: 10m x 10m x 10m, size = 10, dimensions = 3, 2D: 10m x 10m, size = 10, dimensions = 2)
         self.gravCon = 6.67430e-11 # gravitational constant in m^3 kg^-1 s^-2
     
+    # MARK: - Simulation properties setters
     def setDimensions(self, dimensions):
         self.dimensions = dimensions
     
@@ -31,6 +34,7 @@ class Simulation():
         # set the timestep length for the simulation
         self.dt = dt
     
+    # MARK: - Object creation
     def createObject(self, name, mass:float, position:list, velocity:list):        
         # create an object with a random id and the given properties
         while True:
@@ -59,6 +63,7 @@ class Simulation():
         
         return id
     
+    # MARK: - Force and constraint creation
     def createGeneralForce(self, force):
         # create a general force that applies to all objects in the simulation
         self.generalForces.append(force)
@@ -67,6 +72,7 @@ class Simulation():
         # create a constraint that applies to all objects in the simulation
         self.constraints.append(constraint)
     
+    # MARK: - Simulation space creation
     def createSimSpace(self):
         # create the simulation space as a np.array
         # the simulation space is a grid with the size of the simulation space in each dimension of the simulation
@@ -78,24 +84,15 @@ class Simulation():
             position = obj['position']
             self.simSpace[tuple(position)] = id
     
-    def calculateForces(self):
-        # calculate the forces acting on the objects in the simulation
-        # the forces are calculated based on the properties of the objects and the constraints in the simulation
-        # the forces are saved in the objects in the simulation
-        
-        # reset the forces of the objects
-        for id, obj in self.objects.items():
-            obj['forces'] = []
-        
-        # calculate the forces acting on a pair of objects
-        
-    def calculateForce(self, obj1, obj2):
+    # MARK: - Force and acceleration calculation
+    # MARK: - Force calculation
+    def calculateForce(self, id1, id2):
         # calculate the force acting on object 1 by object 2
         # the force is calculated based on the properties of the objects and the constraints in the simulation
         # the force is saved in the object 1 and object 2 in the simulation
         
-        obj1 = self.objects[obj1]
-        obj2 = self.objects[obj2]
+        obj1 = self.objects[id1]
+        obj2 = self.objects[id2]
         
         # calculate the direction of the force
         obj1_position = np.array(obj1['position'])
@@ -106,7 +103,194 @@ class Simulation():
         force = self.gravCon * obj1['mass'] * obj2['mass'] / np.linalg.norm(direction)**2
         
         # save the force in the object 1 and object 2
-        obj1['forces'].append((obj2['id'], direction, force))
-        obj2['forces'].append((obj1['id'], -direction, force))
+        obj1['forces'].append((id2, direction, force))
+        #obj2['forces'].append((id1, -direction, force))
+    
+    def calculateForces(self):
+        # calculate the forces acting on the objects in the simulation
+        # the forces are calculated based on the properties of the objects and the constraints in the simulation
+        # the forces are saved in the objects in the simulation
+        
+        # calculate the forces between all objects in the simulation
+        for id1, obj1 in self.objects.items():
+            for id2, obj2 in self.objects.items():
+                if id1 != id2:
+                    self.calculateForce(id1, id2)
+        
+    
+    # MARK: - Acceleration calculation
+    def calculateAcceleration(self, id):
+        # calculate the acceleration of the object based on the forces acting on the object
+        # the acceleration is calculated based on the properties of the object and the forces acting on the object
+        # the acceleration is saved in the object in the simulation
+        
+        obj = self.objects[id]
+        
+        # calculate the net force acting on the object
+        net_force = np.zeros(self.dimensions)
+        for force in obj['forces']:
+            net_force += force[2] * force[1] / np.linalg.norm(force[1])
+        
+        # calculate the acceleration of the object
+        acceleration = net_force / obj['mass']
+        
+        return acceleration
+    
+    # MARK: - Position update
+    def updatePosition(self, id):
+        # update the position of the object based on the velocity and acceleration of the object
+        # the position is updated based on the properties of the object and the forces acting on the object
+        # the position is saved in the object in the simulation
+        
+        obj = self.objects[id]
+        
+        # calculate the acceleration of the object
+        acceleration = self.calculateAcceleration(id)
+        
+        # update the velocity of the object
+        obj['velocity'] += acceleration * self.dt
+        
+        # update the position of the object
+        obj['position'] += obj['velocity'] * self.dt
+    
+    # MARK: - Space update
+    def updateSpace(self):
+        # update the simulation space with the new positions of the objects
+        # the simulation space is a grid with the size of the simulation space in each dimension of the simulation
+        # the simulation space is used to calculate the forces acting on the objects in the simulation
+        
+        # clear the simulation space
+        self.simSpace = np.zeros(tuple([self.size]*self.dimensions))
+        
+        # add the objects to the simulation space at their new positions
+        for id, obj in self.objects.items():
+            position = obj['position']
+            print('Position:', position)
+            print([int(i) for i in position])
+            self.simSpace[tuple([int(i) for i in position])] = id
+    
+    # MARK: - Simulation cycle
+    def timestep(self):
+        # run one timestep of the simulation
+        # the simulation calculates the forces acting on the objects, updates the positions of the objects, and updates the simulation space
+        
+        # calculate the forces acting on the objects
+        self.calculateForces()
+        
+        # update the positions of the objects
+        for id in self.objects:
+            self.updatePosition(id)
+        
+        # update the simulation space
+        self.updateSpace()
+        #self.display()
+        
+        # increment the timestep
+        self.t += 1
+    
+    # MARK: - Simulation run
+    def run(self):
+        # run the simulation for the specified number of timesteps
+        while self.t < self.t_max:
+            self.timestep()
+            print('Timestep:', self.t)
+            print('Space:','\n', self.simSpace)
+    
+    # MARK: - Save and load simulation
+    # MARK: - Save simulation
+    def createSaveData(self):
+        # create a dictionary with the properties of the simulation to save to a json file
+        data = {
+            'objects': self.objects,
+            'generalForces': self.generalForces,
+            'constraints': self.constraints,
+            'dt': self.dt,
+            't': self.t,
+            't_max': self.t_max,
+            'dimensions': self.dimensions,
+            'size': self.size
+        }
+        
+        return data
+    
+    def save(self, path):
+        # save the simulation to a json file
+        
+        with open(path, 'w') as f:
+            f.write(json.dumps(self.createSaveData(), indent=4))
+    
+    # MARK: - Load simulation
+    def load(self, path):
+        # load the simulation from a json file
+        
+        with open(path, 'r') as f:
+            data = json.load(f)
         
         
+        self.dimensions = data['dimensions']
+        self.size = data['size']
+        self.dt = data['dt']
+        self.t = data['t']
+        self.t_max = data['t_max']
+        self.objects = data['objects']
+        self.generalForces = data['generalForces']
+        self.constraints = data['constraints']
+        
+        self.createSimSpace()
+    
+    # MARK: - Display simulation
+    def display(self):
+        # display the simulation space with the objects in the simulation
+        
+        # create a grid to display the simulation space
+        grid = np.zeros(tuple([self.size]*self.dimensions))
+        
+        # add the objects to the grid at their positions
+        for id, obj in self.objects.items():
+            position = obj['position']
+            grid[[int(i) for i in position]] = 1
+        
+        # display the grid
+        plt.imshow(grid)
+        plt.show()
+    
+
+# MARK: - Main function
+def main():
+    # create a simulation
+    sim = Simulation()
+    
+    # set the dimensions of the simulation
+    sim.setDimensions(2)
+    
+    # set the size of the simulation space
+    sim.setSize(10)
+    
+    # create objects in the simulation
+    sim.createObject('object1', 1, [5, 5], [0, 0])
+    sim.createObject('object2', 1, [3, 3], [0, 0])
+    
+    # create a general force in the simulation
+    sim.createGeneralForce('gravity')
+    
+    # create a constraint in the simulation
+    sim.createConstraint('collision')
+    
+    # create the simulation space
+    sim.createSimSpace()
+    
+    # run the simulation
+    sim.run()
+    
+    # save the simulation
+    #sim.save('simulation.json')
+    
+    # load the simulation
+    #sim.load('simulation.json')
+    
+    # run the simulation
+    #sim.run()
+
+# MARK: - Run main function
+if __name__ == '__main__':
+    main()
