07f505b2c37163a45780b7e9583c0de50c04d7c3
ERR0RW0LF <finn.woma@gmail.com>
2024-06-04 18:54:29 +0200

    Refactor textAnalyses.py for improved symbol analysis and readability

--- test pythone/textAnalyses.py
+++ test pythone/textAnalyses.py
@@ -2,7 +2,8 @@ from pprint import pprint
 import sys
 import os
 
-
+# q: how can i get the path of the file that i am running without the file name
+# a: use os.path.dirname(os.path.realpath(__file__))
 
 def text_analyses(text:str):
     symbols = {
@@ -16,6 +17,19 @@ def text_analyses(text:str):
     
     return symbols
 
+def sort_dict(dict):
+    new_dict = {
+        0: [],
+    }
+    for key in dict:
+        count = int(dict[key])
+        if count in new_dict:
+            new_dict[count].append(key)
+        else:
+            new_dict[count] = []
+            new_dict[count].append(key)
+    return new_dict
+
 if len(sys.argv) >= 2:
     paths = sys.argv[1:]
     symbolsTotal = {}
@@ -26,13 +40,28 @@ if len(sys.argv) >= 2:
                 text = file.read()
                 symbols = text_analyses(text)
                 pprint(symbols)
-                symbolsTotal[path] = symbols
+                for symbol in symbols:
+                    if symbol not in symbolsTotal:
+                        symbolsTotal[symbol] = symbols[symbol]
+                    else:
+                        symbolsTotal[symbol] += symbols[symbol]
 
         else:
             print('File not found')
             sys.exit(1)
     
+    # sort keys by value
+    symbolsTotal = sort_dict(symbolsTotal)
+    
+    print('---'*10)
+    print('Total:')
     pprint(symbolsTotal)
+    
+    # save to file
+    print(os.path.dirname(os.path.realpath(__file__))+'\\symbolsTotal.txt')
+    with open(os.path.dirname(os.path.realpath(__file__))+'\\symbolsTotal.txt', 'w', encoding="utf8") as file:
+        file.write(str(symbolsTotal))
+    
 else:
     print('Usage: python textAnalyses.py <file_path>')
     sys.exit(1)
