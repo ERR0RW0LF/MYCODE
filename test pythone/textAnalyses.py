9bccbc048d832d608c1a24051d9c0af57bea289f
ERR0RW0LF <finn.woma@gmail.com>
2024-06-04 18:22:01 +0200

    Refactor textAnalyses.py for improved symbol analysis and readability

--- test pythone/textAnalyses.py
+++ test pythone/textAnalyses.py
@@ -16,17 +16,23 @@ def text_analyses(text:str):
     
     return symbols
 
-if len(sys.argv) == 2:
-    path = sys.argv[1]
-    if os.path.isfile(path):
-        with open(path, 'r') as file:
-            text = file.read()
-            symbols = text_analyses(text)
-            pprint(symbols)
+if len(sys.argv) >= 2:
+    paths = sys.argv[1:]
+    symbolsTotal = {}
+    for path in paths:
+        print(path)
+        if os.path.isfile(path):
+            with open(path, 'r', encoding="utf8") as file:
+                text = file.read()
+                symbols = text_analyses(text)
+                pprint(symbols)
+                symbolsTotal[path] = symbols
 
-    else:
-        print('File not found')
-        sys.exit(1)
+        else:
+            print('File not found')
+            sys.exit(1)
+    
+    pprint(symbolsTotal)
 else:
     print('Usage: python textAnalyses.py <file_path>')
     sys.exit(1)
