96d39d51365833e8c30d31be6b0405af9411a210
ERR0RW0LF <finn.woma@gmail.com>
2024-06-21 21:56:04 +0200

    Refactor colortest.py to add color code printing and formatting
    Signed-off-by: ERR0RW0LF <finn.woma@gmail.com>

--- test pythone/Terminal/colortest.py
+++ test pythone/Terminal/colortest.py
@@ -13,3 +13,25 @@ def print_format_table():
 
 
 print_format_table()
+
+
+
+import sys
+for i in range(0, 16):
+    for j in range(0, 16):
+        code = str(i * 16 + j)
+        sys.stdout.write(u"\u001b[38;5;" + code + "m " + code.ljust(4))
+    print(u"\u001b[0m")
+
+for i in range(0, 16):
+    for j in range(0, 16):
+        code = str(i * 16 + j)
+        sys.stdout.write(u"\u001b[48;5;" + code + "m " + code.ljust(4))
+    print(u"\u001b[0m")
+
+print(u"\u001b[1m BOLD \u001b[0m\u001b[4m Underline \u001b[0m\u001b[7m Reversed \u001b[0m")
+
+print(u"\u001b[1m\u001b[4m\u001b[7m BOLD Underline Reversed \u001b[0m")
+
+print(u"\u001b[1m\u001b[31m Red Bold \u001b[0m")
+print(u"\u001b[4m\u001b[44m Blue Background Underline \u001b[0m")
