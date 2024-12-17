69dd341b6cf1294a37e5b648023a3850a40e444c
ERR0RW0LF <finn.woma@gmail.com>
2024-12-17 17:58:36 +0100

    Add LaTeX files and SVGs for Lorentz Force representation; refactor lorentz_force.py to correct charge direction and update force arrows

--- LorentzKraft/lorentz_force.py
+++ LorentzKraft/lorentz_force.py
@@ -1,13 +1,12 @@
 from manim import *
 
-class LorentzForce(ThreeDScene):
+class LorentzForce(Scene):
     def construct(self):
         # --- Introduction ---
         title = Tex("The Lorentz Force").scale(1.5)
         self.play(Write(title))
         self.wait(1)
         self.play(FadeOut(title))
-
 
         # --- Electric Field ---
         electric_field_text = Tex("Electric Field (E)").to_corner(UP + LEFT)
@@ -30,21 +29,19 @@ class LorentzForce(ThreeDScene):
         self.play(Create(magnetic_field_dots))
         self.wait(1)
 
-        # --- Moving Charged Particle ---
-        charged_particle = Dot(color=RED, radius=0.2)
+        # --- Moving Charged Particle (Electron) ---
+        charged_particle = Dot(color=BLUE, radius=0.2)
         charged_particle.move_to([-4,0,0])
-        charge_label = Tex("+q").scale(0.7).next_to(charged_particle, RIGHT)
+        charge_label = Tex("-e").scale(0.7).next_to(charged_particle, RIGHT)
         self.play(Create(charged_particle),Write(charge_label))
-
 
         self.wait(1)
 
         # --- Electric Force Only ---
-
         electric_force_text = Tex("Electric Force (F$_E$)").to_edge(DOWN+LEFT)
         self.play(Write(electric_force_text))
-        self.play(charged_particle.animate.shift([4,0,0]))
-        electric_force_arrow=Arrow(start=charged_particle.get_center(),end = charged_particle.get_center() +[4,0,0], color= YELLOW)
+        self.play(charged_particle.animate.shift([-4,0,0])) # Changed direction
+        electric_force_arrow=Arrow(start=charged_particle.get_center(),end = charged_particle.get_center() +[-4,0,0], color= YELLOW) # Changed direction
         self.play(Create(electric_force_arrow))
         self.wait(1)
         self.play(FadeOut(electric_force_arrow),FadeOut(electric_force_text))
@@ -52,11 +49,10 @@ class LorentzForce(ThreeDScene):
         # --- Magnetic Force Only ---
         magnetic_force_text = Tex("Magnetic Force (F$_B$)").to_edge(DOWN+RIGHT)
         self.play(Write(magnetic_force_text))
-        self.play(charged_particle.animate.shift([-4,0,0]))
-        self.move_camera(phi =70*DEGREES,theta = 180*DEGREES)
-        self.play(Rotate(charged_particle,angle = PI/2, axis = OUT), rate_func = smooth)
+        self.play(charged_particle.animate.shift([4,0,0])) # Changed direction
 
-        magnetic_force_arrow = Arrow(start = charged_particle.get_center(), end = charged_particle.get_center() + [0,3,0], color = BLUE)
+        # Now the magnetic force will be inwards from the screen (due to negative charge)
+        magnetic_force_arrow = Arrow(start = charged_particle.get_center(), end = charged_particle.get_center() + [0,0,-0.5] , color = BLUE).rotate(PI/2,axis = RIGHT) # Changed direction
         self.play(Create(magnetic_force_arrow))
 
         self.wait(1)
@@ -66,21 +62,16 @@ class LorentzForce(ThreeDScene):
         lorentz_force_text = Tex("Lorentz Force (F)").to_edge(DOWN)
         self.play(Write(lorentz_force_text))
 
-        self.move_camera(phi = 0, theta = 0)
-        self.play(Rotate(charged_particle, angle= -PI/2, axis = OUT), rate_func = smooth)
+        self.play(charged_particle.animate.shift([-4,0,0]))# Changed direction
 
-        self.play(charged_particle.animate.shift([4,0,0]))
-
-        lorentz_force_arrow = Arrow(start = charged_particle.get_center(), end = charged_particle.get_center() + [4, 3, 0], color=GREEN)
+        lorentz_force_arrow = Arrow(start = charged_particle.get_center(), end = charged_particle.get_center() + [-4, -0.5, -0.5], color=GREEN).rotate(PI/2, axis=RIGHT) # Changed direction
         self.play(Create(lorentz_force_arrow))
         self.wait(1)
 
-        # --- Rule of Thumb
-        hand_text = Tex("Right-Hand Rule").next_to(lorentz_force_arrow,DOWN)
-        self.play(Write(hand_text))
+        self.play(FadeOut(lorentz_force_arrow), FadeOut(lorentz_force_text), FadeOut(electric_field_lines),
+                    FadeOut(magnetic_field_dots),FadeOut(charge_label),FadeOut(charged_particle),FadeOut(magnetic_field_text),
+                    FadeOut(electric_field_text))
 
-        self.wait(2)
-        self.play(FadeOut(lorentz_force_arrow), FadeOut(hand_text), FadeOut(lorentz_force_text), FadeOut(electric_field_lines),FadeOut(magnetic_field_dots),FadeOut(charge_label),FadeOut(charged_particle),FadeOut(magnetic_field_text),FadeOut(electric_field_text))
 
         # --- Conclusion ---
         conclusion = Tex("Lorentz Force: F = q(E + v x B)").scale(1.2)
