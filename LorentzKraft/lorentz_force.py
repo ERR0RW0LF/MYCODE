059b07e8ba5eb65638211c2c91d0f5852a1a335e
ERR0RW0LF <finn.woma@gmail.com>
2024-12-17 23:08:28 +0100

    Update Lorentz Force animation: correct charge direction and modify partial movie file list

--- LorentzKraft/lorentz_force.py
+++ LorentzKraft/lorentz_force.py
@@ -40,8 +40,8 @@ class LorentzForce(Scene):
         # --- Electric Force Only ---
         electric_force_text = Tex("Electric Force (F$_E$)").to_edge(DOWN+LEFT)
         self.play(Write(electric_force_text))
-        self.play(charged_particle.animate.shift([-4,0,0])) # Changed direction
-        electric_force_arrow=Arrow(start=charged_particle.get_center(),end = charged_particle.get_center() +[-4,0,0], color= YELLOW) # Changed direction
+        self.play(charged_particle.animate.shift([-4,0,0]))
+        electric_force_arrow=Arrow(start=charged_particle.get_center(),end = charged_particle.get_center() +[-4,0,0], color= YELLOW)
         self.play(Create(electric_force_arrow))
         self.wait(1)
         self.play(FadeOut(electric_force_arrow),FadeOut(electric_force_text))
@@ -49,10 +49,10 @@ class LorentzForce(Scene):
         # --- Magnetic Force Only ---
         magnetic_force_text = Tex("Magnetic Force (F$_B$)").to_edge(DOWN+RIGHT)
         self.play(Write(magnetic_force_text))
-        self.play(charged_particle.animate.shift([4,0,0])) # Changed direction
+        self.play(charged_particle.animate.shift([4,0,0]))
 
         # Now the magnetic force will be inwards from the screen (due to negative charge)
-        magnetic_force_arrow = Arrow(start = charged_particle.get_center(), end = charged_particle.get_center() + [0,0,-0.5] , color = BLUE).rotate(PI/2,axis = RIGHT) # Changed direction
+        magnetic_force_arrow = Arrow(start = charged_particle.get_center(), end = charged_particle.get_center() + [0,0,-0.5] , color = BLUE).rotate(PI/2,axis = RIGHT)
         self.play(Create(magnetic_force_arrow))
 
         self.wait(1)
@@ -62,9 +62,10 @@ class LorentzForce(Scene):
         lorentz_force_text = Tex("Lorentz Force (F)").to_edge(DOWN)
         self.play(Write(lorentz_force_text))
 
-        self.play(charged_particle.animate.shift([-4,0,0]))# Changed direction
+        self.play(charged_particle.animate.shift([-4,0,0]))
 
-        lorentz_force_arrow = Arrow(start = charged_particle.get_center(), end = charged_particle.get_center() + [-4, -0.5, -0.5], color=GREEN).rotate(PI/2, axis=RIGHT) # Changed direction
+
+        lorentz_force_arrow = Arrow(start = charged_particle.get_center(), end = charged_particle.get_center() + [-4, 0.5, -0.5], color=GREEN).rotate(PI/2, axis=RIGHT)  # Corrected direction here
         self.play(Create(lorentz_force_arrow))
         self.wait(1)
 
